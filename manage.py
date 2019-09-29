17e4fefcbae9cd778c0ef23ee96f01cbe0773f0b
seunghaekim <aka.seunghaekim@gmail.com>
2019-09-29 22:48:20 +0900

    MODIFY simplize import lists

--- manage.py
+++ manage.py
@@ -4,20 +4,16 @@ from lib.app import app
 from lib.app import api
 from lib.db import db
 
-from pages.models import *
-from pages.api import PagesWithoutId
-from pages.api import Pages
-from pages.api import PagesWithSlug
-from pages.api import PostsWithoutId
-from pages.api import Posts
-from pages.api import PostsWithSlug
+import pages.models
+import pages.api as PagesApi
+import users.models
 
-api.add_resource(PagesWithoutId, '/pages/')
-api.add_resource(Pages, '/pages/<int:page_id>')
-api.add_resource(PagesWithSlug, '/pages/<string:slug>')
-api.add_resource(PostsWithoutId, '/posts/')
-api.add_resource(Posts, '/posts/<int:page_id>')
-api.add_resource(PostsWithSlug, '/posts/<string:slug>')
+api.add_resource(PagesApi.PagesWithoutId, '/pages/')
+api.add_resource(PagesApi.Pages, '/pages/<int:page_id>')
+api.add_resource(PagesApi.PagesWithSlug, '/pages/<string:slug>')
+api.add_resource(PagesApi.PostsWithoutId, '/posts/')
+api.add_resource(PagesApi.Posts, '/posts/<int:page_id>')
+api.add_resource(PagesApi.PostsWithSlug, '/posts/<string:slug>')
 
 migrate = Migrate(app, db)
 
